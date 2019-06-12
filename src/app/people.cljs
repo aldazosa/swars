5346170b0f53c3b56e62592b2720833dfcfc8b94
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2019-06-12 16:11:16 -0500

    Lista los nombres de los personajes.

--- src/app/people.cljs
+++ src/app/people.cljs
@@ -1,13 +1,38 @@
 (ns app.people
-  (:require ["@smooth-ui/core-sc" :refer [Box Typography]]))
+  (:require
+   [re-frame.core :refer [reg-sub]]
+   [app.common :refer [<sub]]
+   ["@smooth-ui/core-sc" :refer [Box Typography]]))
+
+
+(reg-sub
+  :characters
+  (fn [db _]
+   (vals (get-in db [:people]))))
+
+
+(defn character-card
+  [character]
+  [:> Box (:name character)])
+
+
+(defn character-list
+  [characters]
+  [:> Box
+   (for [character characters]
+     ^{:key (:id character)}
+     [character-card character])])
 
 
 (defn people-page
   []
-  [:<>
-   [:> Box {:display         "flex"
-            :justify-content "center"}
-    [:> Typography {:variant     "h2"
-                    :py          20
-                    :font-weight 700}
-     "Personajes"]]])
+  (let [chars (<sub [:characters])]
+    [:<>
+     [:> Box {:display         "flex"
+              :justify-content "center"}
+      [:> Typography {:variant     "h2"
+                      :py          20
+                      :font-weight 700}
+       "Personajes"]]
+
+     [character-list chars]]))
